25fac5c1d8c0c47cf944c3be1350505a081e3166
christianjason <cornelchristian18@gmail.com>
2025-02-18 23:03:01 +0800

    cj-weather-app

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Quezon City",
+    country: "PH",
+    state: "Metro Manila",
+    lat: 14.6760,
+    lon: 121.0437,
+  },
+  {
+    name: "Cebu City",
+    country: "PH",
+    state: "Cebu",
+    lat: 10.3157,
+    lon: 123.8854,
+  },
+  {
+    name: "Davao City",
+    country: "PH",
+    state: "Davao del Sur",
+    lat: 7.1907,
+    lon: 125.4553,
+  },
+  {
+    name: "Makati",
+    country: "PH",
+    state: "Metro Manila",
+    lat: 14.5547,
+    lon: 121.0244,
+  },
+  {
+    name: "Iloilo City",
+    country: "PH",
+    state: "Iloilo",
+    lat: 10.7202,
+    lon: 122.5613,
+  },
+];
+
+export default defaultStates;
